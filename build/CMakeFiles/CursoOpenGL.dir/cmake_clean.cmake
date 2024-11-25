44fa283bccb8e7b1f2b3e4c870b7670e021eb8f0
MartinZayasZ <zayasmartin70@gmail.com>
2024-11-25 03:42:50 -0700

    first commit

---
+++ build/CMakeFiles/CursoOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CursoOpenGL.dir/src/glad.c.o"
+  "CMakeFiles/CursoOpenGL.dir/src/glad.c.o.d"
+  "CMakeFiles/CursoOpenGL.dir/src/main.cpp.o"
+  "CMakeFiles/CursoOpenGL.dir/src/main.cpp.o.d"
+  "CursoOpenGL"
+  "CursoOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CursoOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
